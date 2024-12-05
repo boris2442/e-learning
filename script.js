062b3608c94e2e837b54d932e8eb1b9d7d2a793a
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-05 07:31:07 +0100

    introduction du javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+const titre=document.getElementById("title");
+new Typewriter(titre,
+    {
+        loop:true,
+        delay:150
+        // deleteSpeed:1000
+    })
+    // typewriter
+    .typeString("Bienvenue chez Boris Fashion")
+    .pauseFor(2000)
+    .deleteAll(500)
+    .pauseFor(2000)
+    .typeString(" <span style='fontFamily:'parisienne'' >Votre fournisseur par excellence   en matiere du shopping </span>")
+    .pauseFor(1000)
+    .start();
+
